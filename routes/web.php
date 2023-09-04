96f724ec206553140dd05c6524606f82ea09ea77
Shiva Guru Balaji S <shivagurubalaji@gmail.com>
2023-09-05 01:19:36 +0530

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/breadcrumb',[SimpleFunctionsController::class,"create_breadcrumbs"]);
+Route::get('/breadcrumb', 'SimpleFunctionsController@create_breadcrumbs');
