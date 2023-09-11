214e8e3178f54ffee7f968195fb085e34b68890f
Shiva Guru Balaji S <shivagurubalaji@gmail.com>
2023-09-11 12:40:46 +0530

    fix: global header

---
+++ resources/views/blocks/header/global-header.blade.php
@@ -0,0 +1,74 @@
+<title>{{ $get_index_data->page_title ?? 'N/A' }} | FP Markets English</title>
+    
+<base href="{{env('APP_URL', 'https://www.fpmarkets.com/sc')}}ae-en/" />
+<meta charset="UTF-8"/>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Bootstrap CSS -->
+<!-- Latest compiled and minified CSS -->
+<link href="/assets/templates/bootstrap.min.css" rel="stylesheet">
+
+<link href="/assets/templates/v3/assets/css/all.min.css" rel="stylesheet">
+<script src="https://use.fontawesome.com/44325f843e.js"></script>
+<link href="/assets/templates/v3/assets/css/owl.carousel.min.css" rel="stylesheet">
+<link href="/assets/templates/v3/assets/css/simplyScroll.css?v=1.4" rel="stylesheet">
+<link href="/assets/templates/v3/assets/css/style.css?v=3.1.5" rel="stylesheet">
+<link href="/assets/templates/v3/assets/css/style_v3.css?v=2.5" rel="stylesheet">
+<link href="/assets/templates/v3/assets/css/responsive.css?v=1.6" rel="stylesheet">
+<link href="/assets/templates/v3/assets/css/new.css?v=7" rel="stylesheet">
+
+
+    <link rel="alternate" hreflang="en" href="https://www.fpmarkets.com/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="en-AU" href="https://www.fpmarkets.com/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="en-GB" href="https://www.fpmarkets.com/uk/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="en-ZA" href="https://www.fpmarkets.com/za/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="en-IE" href="https://www.fpmarkets.com/ie/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="pt-BR" href="https://www.fpmarkets.com/br/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="pt" href="https://www.fpmarkets.com/br/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="en-CA" href="https://www.fpmarkets.com/ca/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="cs-CZ" href="https://www.fpmarkets.com/cz/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="cs" href="https://www.fpmarkets.com/cz/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="zh-CN" href="https://www.fpmarkets.com/cn/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="zh" href="https://www.fpmarkets.com/cn/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="de-DE" href="https://www.fpmarkets.com/de/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="de" href="https://www.fpmarkets.com/de/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="es-ES" href="https://www.fpmarkets.com/es/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="fr-FR" href="https://www.fpmarkets.com/fr/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="fr" href="https://www.fpmarkets.com/fr/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="el" href="https://www.fpmarkets.com/gr/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="id-ID" href="https://www.fpmarkets.com/id/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="id" href="https://www.fpmarkets.com/id/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="ar" href="https://www.fpmarkets.com/ar/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="it-IT" href="https://www.fpmarkets.com/it/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="it" href="https://www.fpmarkets.com/it/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="sw-KE" href="https://www.fpmarkets.com/ke/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="sw" href="https://www.fpmarkets.com/ke/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="ko-KR" href="https://www.fpmarkets.com/ko/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="ko" href="https://www.fpmarkets.com/ko/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="ms-MY" href="https://www.fpmarkets.com/ms/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="ms" href="https://www.fpmarkets.com/ms/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="pt-PT" href="https://www.fpmarkets.com/pt/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="pl-PL" href="https://www.fpmarkets.com/pl/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="pl" href="https://www.fpmarkets.com/pl/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="th" href="https://www.fpmarkets.com/th/[[*duplicateUri]]" />
+    <link rel="alternate" hreflang="vi" href="https://www.fpmarkets.com/vi/[[*duplicateUri]]" />
+
+    <link rel="canonical" href="https://www.fpmarkets.com/{{ $get_index_data->page_url ?? 'N/A' }}" />
+
+
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"
+integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJQ6RQ');</script>
+<!-- End Google Tag Manager -->
