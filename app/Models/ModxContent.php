95d9ab440a2dfa55be5dcb466f6ed1dda18efc0d
Shiva Guru Balaji S <shivagurubalaji@gmail.com>
2023-09-11 11:34:48 +0530

    fix: ModxContent

--- app/Models/ModxContent.php
+++ app/Models/ModxContent.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ModxContent extends Model
 {
-    use HasFactory;
-
     protected $table = 'modx_site_content';
 
     protected $fillable = [
